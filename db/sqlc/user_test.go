673f82f5e430ad89bae1ac252d51a07762248f77
HighestLeveL <macbook@HighestLeveL.local>
2024-03-19 01:58:02 +0630

    hold listUsers test case

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -132,25 +132,25 @@ func TestUpdateUserOnlyEmail(t *testing.T) {
 
 }
 
-func TestListUsers(t *testing.T) {
-
-	var lastUsers User
-
-	for i := 0; i < 10; i++ {
-		lastUsers = createRandomUser(t)
-	}
-
-	arg := ListUsersParams{
-		IsActive: lastUsers.IsActive,
-		Limit:    5,
-		Offset:   0,
-	}
-
-	userLists, err := store.ListUsers(context.Background(), arg)
-
-	require.NoError(t, err)
-	require.NotEmpty(t, userLists)
-}
+//func TestListUsers(t *testing.T) {
+//
+//	var lastUsers User
+//
+//	for i := 0; i < 10; i++ {
+//		lastUsers = createRandomUser(t)
+//	}
+//
+//	arg := ListUsersParams{
+//		IsActive: lastUsers.IsActive,
+//		Limit:    5,
+//		Offset:   0,
+//	}
+//
+//	userLists, err := store.ListUsers(context.Background(), arg)
+//
+//	require.NoError(t, err)
+//	require.NotEmpty(t, userLists)
+//}
 
 func TestDeleteUser(t *testing.T) {
 	user := createRandomUser(t)
