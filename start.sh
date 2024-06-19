c6b3f795a5af8894ad43bc500a97b468a7ef8394
KyawKyawThar <kyawkyaw.thar84@gmail.com>
2024-06-19 16:21:51 +0630

    db migration version update

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#in alpine image bashshell is not abailable
 
 set -e
 
@@ -9,4 +10,5 @@ source /app/app.env
 /app/db/migrate -path /app/db/migrations -database "$DB_SOURCE" -verbose up
 echo "start the app"
 
+# take all parameter passed to the script and run it
 exec "$@"
